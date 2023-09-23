8e78705da8a7126f7da113a5eeebb94d6139fab0
Franco Martin Paturlanne <paturlannefm@gmail.com>
2023-09-23 14:42:03 -0300

    Test and code checkbox functionality

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -18,3 +18,29 @@ test("button has the correct initial color, and updates when clicked", () => {
   //expect the button text to be "Change to red"
   expect(colorButton).toHaveTextContent("Change to red");
 });
+
+test("initial conditions", () => {
+  render(<App />);
+
+  //check that the button start out enabled
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+
+  //check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("Checkbox disables button on first click and enables on second click", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox");
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  //check that when user click the checkbox the button change to disabled
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+
+  //check than when user click again the checkbox the button change to enabled
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
